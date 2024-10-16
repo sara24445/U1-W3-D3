59f3beef4e47cb52ac3eb7b68803117227a86945
Sara Severgnini <sara.severgnini.05@gmail.com>
2024-10-16 16:22:07 +0200

    new

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const form = document.getElementById('main-form')
 form.addEventListener('submit', function name(e) {
   e.preventDefault()
+  console.log('INVIO DEL FORM', e)
+  const taginput = document.getElementById('tag')
+  const tagvalue = taginput.value
+  console.log('tag', tagvalue)
+  const newH3 = document.createElement('h3')
+  newH3.innerText = 'Grazie! Prodotto registrato correttamente.'
+  const main = document.getElementsByTagName('main')[0]
+  main.appendChild(newH3)
+
+  taginput.value = ''
+  form.reset()
 })
